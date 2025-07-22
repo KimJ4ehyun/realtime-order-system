366afb908e0a095256de566d695b67679d180fd2
KimJ4ehyun <kimjaehyun158@gmail.com>
2025-07-22 13:34:15 +0900

    Refactor: unused repository 삭제

--- order-service/src/main/java/com/example/orderservice/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.orderservice.repository;
-
-import com.example.orderservice.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
