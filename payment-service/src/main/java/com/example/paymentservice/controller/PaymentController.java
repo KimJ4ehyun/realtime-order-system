686c07b67856ccaba6b649958bc6bf3b96b4f8dc
KimJ4ehyun <kimjaehyun158@gmail.com>
2025-07-25 18:18:38 +0900

    Refactor: url 변경

--- payment-service/src/main/java/com/example/paymentservice/controller/PaymentController.java
+++ payment-service/src/main/java/com/example/paymentservice/controller/PaymentController.java
@@ -7,15 +7,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/payments")
 @RequiredArgsConstructor
 public class PaymentController {
 
     private final PaymentService paymentService;
 
-    @PostMapping("/orders/{orderId}/payments")
+    @PostMapping("/orders/{orderId}")
     public ResponseEntity<PaymentResponse> processPayment(@PathVariable Long orderId) {
         return ResponseEntity.status(HttpStatus.CREATED).body(paymentService.processPayment(orderId));
     }
